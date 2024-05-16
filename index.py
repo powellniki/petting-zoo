8ea2f55519a9278a437c633c6578f7f9199e0a42
Niki Powell <nhe.powell@gmail.com>
2024-05-16 15:59:45 -0500

    food function fun

--- index.py
+++ index.py
@@ -3,23 +3,25 @@ from swimming import Frog, Goldfish, Goose, Mallard, Turtle
 from walking import Donkey, Goat, Horse, Llama, Pig
 
 
-spot = Copperhead("Spot", "copperhead", "tank")
-charlie = RatSnake("Charlie", "rat snake", "tank")
-cookie = Lizard("Cookie", "lizard", "tank")
-bubbles = Gecko("Bubbles", "gecko", "tank")
-viper = Salamander("Viper", "salamander", "tank")
+spot = Copperhead("Spot", "copperhead", "tank", "mice")
+charlie = RatSnake("Charlie", "rat snake", "tank", "mice")
+cookie = Lizard("Cookie", "lizard", "tank", "crickets")
+bubbles = Gecko("Bubbles", "gecko", "tank", "bugs")
+viper = Salamander("Viper", "salamander", "tank", "salamander chow")
 
-alien = Mallard("Alien", "mallard", "pond")
-funky = Goldfish("Funky", "goldfish", "pond")
-jonathan = Turtle("Jonathan", "turtle", "pond")
-sniper = Goose("Sniper", "goose", "pond")
-legs = Frog("Legs", "frog", "pond")
+alien = Mallard("Alien", "mallard", "pond", "bread chunks")
+funky = Goldfish("Funky", "goldfish", "pond", "fish flakes")
+jonathan = Turtle("Jonathan", "turtle", "pond", "turtle bites")
+sniper = Goose("Sniper", "goose", "pond", "goose flakes")
+legs = Frog("Legs", "frog", "pond", "bugs")
 
-frank = Llama("Frank the Tank", "llama", "petting area", "midday")
-tina = Goat("Tina", "goat", "petting area", "morning")
-nancy = Pig("Nancy", "pig", "petting area", "afternoon")
-alfred = Donkey("Alfred", "donkey", "petting area", "morning")
-susan = Horse("Susan", "horse", "petting area", "afternoon")
+frank = Llama("Frank the Tank", "llama", "petting area", "midday", "llama chow")
+tina = Goat("Tina", "goat", "petting area", "morning", "goat pellets")
+nancy = Pig("Nancy", "pig", "petting area", "afternoon", "chilled mud")
+alfred = Donkey("Alfred", "donkey", "petting area", "morning", "hay")
+susan = Horse("Susan", "horse", "petting area", "afternoon", "hay")
 
 
-print(f"{frank.name} the {frank.species} is available to pet during the {frank.shift} shift")
+# print(f"{frank.name} the {frank.species} is available to pet during the {frank.shift} shift")
+
+# print(jonathan.feed())
--- walking/pig.py
+++ walking/pig.py
@@ -2,7 +2,7 @@
 from datetime import date
 
 class Pig():
-    def __init__(self, name, species, category, shift):
+    def __init__(self, name, species, category, shift, food):
         # Establish the properties of each animal
         # with a default value
         self.name = name
@@ -11,6 +11,10 @@ class Pig():
         self.category = category
         self.walking = True
         self.shift = shift
+        self.food = food
 
     def __str___(self):
         return f"{self.name} the {self.species} is available to pet during the {self.shift} shift"
+    
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%y")}')
